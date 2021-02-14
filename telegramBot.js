36c40521f3f26ed463a3be9b43c8041243766c7b
dannyang27 <danny27995@gmail.com>
2021-02-14 15:34:38 +0000

    remove_commented_code

--- telegramBot.js
+++ telegramBot.js
@@ -1,14 +1,3 @@
-// const TelegramBot = require("node-telegram-bot-api");
-// const token = "1640720531:AAEj-A2KP2hRP1ltRHdar5IWtzfG-_NLtxU"
-// const bot = new TelegramBot(token, {polling: true});
-// const chatId = 920224495
-//
-// function sendMessage() {
-//     // const date = new Date().toLocaleString()
-//     const helloBot = "A wild recruiter is visiting your page ";
-//     bot.sendMessage(920224495, helloBot);
-// }
-
 const TELEGRAM_BOT_TOKEN = "1640720531:AAEj-A2KP2hRP1ltRHdar5IWtzfG-_NLtxU"
 
 const TeleBot = require('telebot');
