ed25092feaca8436ddc7e355b33511e88e241a2a
dannyang27 <danny27995@gmail.com>
2021-02-14 22:58:43 +0000

    update_bot_message

--- bundle.js
+++ bundle.js
@@ -40983,25 +40983,14 @@ WError.prototype.cause = function we_cause(c)
 };
 
 },{"assert-plus":49,"core-util-is":56,"extsprintf":62,"util":423}],170:[function(require,module,exports){
-// const TelegramBot = require("node-telegram-bot-api");
-// const token = "1640720531:AAEj-A2KP2hRP1ltRHdar5IWtzfG-_NLtxU"
-// const bot = new TelegramBot(token, {polling: true});
-// const chatId = 920224495
-//
-// function sendMessage() {
-//     // const date = new Date().toLocaleString()
-//     const helloBot = "A wild recruiter is visiting your page ";
-//     bot.sendMessage(920224495, helloBot);
-// }
-
 const TELEGRAM_BOT_TOKEN = "1640720531:AAEj-A2KP2hRP1ltRHdar5IWtzfG-_NLtxU"
 
 const TeleBot = require('telebot');
 const bot = new TeleBot(TELEGRAM_BOT_TOKEN);
 
 const chatId = 920224495
-const date = new Date().toLocaleString()
-const helloBot = "A wild recruiter is visiting your page at " + date;
+const date = new Date().toISOString()
+const helloBot = "A wild user is visiting your page at " + date;
 
 bot.sendMessage(chatId, helloBot);
 },{"telebot":135}],171:[function(require,module,exports){
